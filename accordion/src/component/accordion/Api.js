25c70cc1f3b6562648d6cdab6bb4d4150a3aed33
ganshika <ganshika@bridgetree.com>
2022-01-05 13:46:15 +0530

    Accordion

---
+++ accordion/src/component/accordion/Api.js
@@ -0,0 +1,37 @@
+export const questions= [{
+
+    id: 1,
+    question:'What is React?',
+    answer:'React is a declarative, efficient, flexible open source front-end JavaScript library developed by Facebook in 2011. It follows the component-based approach for building reusable UI components, especially for single page application.',
+},
+
+{
+
+    id: 2,
+    question:'What is JSX?',
+    answer:'JSX stands for JavaScript XML. It is a React extension which allows writing JavaScript code that looks similar to HTML. It makes HTML file easy to understand.',
+},
+
+{
+
+    id: 3,
+    question:'What do you understand by Virtual DOM?',
+    answer:'A Virtual DOM is a lightweight JavaScript object which is an in-memory representation of real DOM. ',
+},
+
+{
+
+    id: 4,
+    question:'Explain the purpose of render() in React.',
+    answer:'It is mandatory for each React component to have a render() function. Render function is used to return the HTML which you want to display in a component.  ',
+},
+
+{
+
+    id: 5,
+    question:'What is Props?',
+    answer:'Props stand for "Properties" in React. They are read-only inputs to components. Props are an object which stores the value of attributes of a tag and work similar to the HTML attributes. ',
+}
+
+
+]
